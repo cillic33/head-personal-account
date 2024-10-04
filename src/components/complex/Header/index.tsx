f6e92b4364a4e23ac10de26c693d87989c1b4527
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-10-04 20:48:46 +0700

    master | AvatarIvanov added

--- src/components/complex/Header/index.tsx
+++ src/components/complex/Header/index.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import Flexbox from "@components/surfaces/Flexbox";
-import LogoIbs from "@images/LogoIbs.svg"
 import Image from "@components/data-display/Image";
 import Typography from "@components/data-display/Typography";
+import LogoIbs from "@images/LogoIbs.svg"
+import AvatarIvanov from "@images/AvatarIvanov.png"
 
 const StyledHeader = styled.div`
   background: #fff;
@@ -13,13 +14,21 @@ const Header = () => {
   return (
     <StyledHeader>
       <Flexbox $align="center" $justify="space-between">
+
         <Flexbox $gap="24px" $align="center">
           <Image src={LogoIbs} alt="Главная" $width="40px" $height="40px" />
           <Typography $variant="h3">
             Личный кабинет руководителя
           </Typography>
         </Flexbox>
-        <div>Иванов Сергей Владимирович</div>
+
+        <Flexbox $gap="8px" $align="center">
+          <Image src={AvatarIvanov} alt="Иванов Сергей Владимирович" $width="40px" $height="40px" />
+          <Typography $variant="body-regular">
+            Иванов Сергей Владимирович
+          </Typography>
+        </Flexbox>
+
       </Flexbox>
     </StyledHeader>
   );
