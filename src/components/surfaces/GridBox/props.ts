9d8e3ec2fb42bc7f5d46c800b27f59346a7be194
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-11-15 17:11:37 +0700

    Merge branch 'code-review-notices'
    # Conflicts:
    #	src/components/complex/EmployeeCard/index.tsx
    #	src/components/data-display/Link/index.tsx
    #	src/components/data-display/Link/props.ts
    #	src/components/surfaces/Expand/index.tsx
    #	src/components/surfaces/Expand/props.ts
    #	src/components/surfaces/Gridbox/index.tsx
    #	src/components/surfaces/Gridbox/props.ts

--- src/components/surfaces/GridBox/props.ts
+++
@@ -1,9 +0,0 @@
-import {ReactNode} from "react";
-
-export interface IProps {
-  children: ReactNode;
-  gridTemplateColumns?: string;
-  gridTemplateRows?: string;
-  rowGap?: string;
-  columnGap?: string;
-}
